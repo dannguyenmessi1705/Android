1ba0114943f24c3e4a07b6dfb059d91d331d4541
dannguyenmessi1705 <didannguyen17@gmail.com>
2026-01-22 01:30:15 +0700

    fix: Correct the base URL in RetrofitInstance
    Corrects a typo in the `baseUrl` string within the `RetrofitInstance` class, changing `http//` to `http://`.

--- QuizApp/app/src/main/java/com/didan/android/retrofit/quizapp/retrofit/RetrofitInstance.kt
+++ QuizApp/app/src/main/java/com/didan/android/retrofit/quizapp/retrofit/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
-    val baseUrl = "http//localhost:80/quiz/"
+    val baseUrl = "http://localhost:80/quiz/"
 
     fun getRetrofitInstance(): Retrofit {
         // Implementation for creating and returning a Retrofit instance
